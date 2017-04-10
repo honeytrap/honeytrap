0a895f87218fb6982304b50b0db129a0f2c2e4e8
Alexander Ewetumo <trinoxf@gmail.com>
2017-04-10 18:33:37 +0100

    Add tests for websocket api for honeycast
    This commit has basic unit tests for the honeycast /ws endpoint API,
    we hope to be able to validate the responses  and that the behaviour
    matches the expected outcome.

--- server/api_test.go
+++ server/api_test.go
@@ -9,9 +9,11 @@ import (
 	"testing"
 
 	assetfs "github.com/elazarl/go-bindata-assetfs"
+	"github.com/gorilla/websocket"
 	"github.com/honeytrap/honeytrap/config"
 	"github.com/honeytrap/honeytrap/pushers/message"
 	"github.com/honeytrap/honeytrap/server"
+	"github.com/influx6/faux/tests"
 
 	web "github.com/honeytrap/honeytrap-web"
 )
@@ -470,5 +472,126 @@ func TestHoneycastFiltering(t *testing.T) {
 			}
 			t.Logf("\t%s\t Should have total of 3 events in store.", passed)
 		}
+
+		t.Logf("\t When connecting with websocket to the /ws endpoints ")
+		{
+
+			conn, _, err := connectWS(sm.URL+"/ws", nil)
+			if err != nil {
+				tests.Failed("Should have successfully connected with a websocket client")
+			}
+			tests.Passed("Should have successfully connected with a websocket client")
+
+			defer conn.WriteMessage(websocket.CloseMessage, nil)
+			defer conn.Close()
+
+			t.Logf("\t When retrieving all sessions from websocket connection")
+			{
+				if serr := send(conn, server.FetchSessions, nil); serr != nil {
+					tests.Failed("Should have successfully delivered message to a websocket server")
+				}
+				tests.Passed("Should have successfully delivered message to a websocket server")
+
+				response, err := read(conn)
+				if err != nil {
+					tests.Failed("Should have successfully read response from websocket client")
+				}
+				tests.Passed("Should have successfully read response from websocket client")
+
+				if response.Type != server.FetchSessionsReply {
+					t.Logf("\t\t Response: %+#q", response)
+					tests.Failed("Should have successfully received FetchSessionReply from websocket server")
+				}
+				tests.Passed("Should have successfully received FetchSessionReply from websocket server")
+			}
+
+			t.Logf("\t When retrieving all events from websocket connection")
+			{
+				if serr := send(conn, server.FetchEvents, nil); serr != nil {
+					tests.Failed("Should have successfully delivered message to a websocket server")
+				}
+				tests.Passed("Should have successfully delivered message to a websocket server")
+
+				response, err := read(conn)
+				if err != nil {
+					tests.Failed("Should have successfully read response from websocket client")
+				}
+				tests.Passed("Should have successfully read response from websocket client")
+
+				if response.Type != server.FetchEventsReply {
+					t.Logf("\t\t Response: %+#q", response)
+					tests.Failed("Should have successfully received FetchEventsReply from websocket server")
+				}
+				tests.Passed("Should have successfully received FetchEventsReply from websocket server")
+			}
+
+			t.Logf("\t When retrieving an unknown event type '20' from websocket connection")
+			{
+				if serr := send(conn, server.MessageType(20), nil); serr != nil {
+					tests.Failed("Should have successfully delivered message to a websocket server")
+				}
+				tests.Passed("Should have successfully delivered message to a websocket server")
+
+				response, err := read(conn)
+				if err != nil {
+					tests.Failed("Should have successfully read response from websocket client")
+				}
+				tests.Passed("Should have successfully read response from websocket client")
+
+				if response.Type != server.ErrorResponse {
+					t.Logf("\t\t Response: %+#q", response)
+					tests.Failed("Should have successfully received ErrorResponse from websocket server")
+				}
+				tests.Passed("Should have successfully received ErrorResponse from websocket server")
+			}
+
+		}
 	}
+}
+
+var dailer = websocket.Dialer{}
+
+// connectWS connects to the giving URL to create a websocket connection.
+func connectWS(url string, headers map[string]string) (*websocket.Conn, *http.Response, error) {
+	header := make(http.Header)
+
+	for key, val := range headers {
+		header.Set(key, val)
+	}
+
+	return dailer.Dial(url, header)
+}
+
+func send(conn *websocket.Conn, messageType server.MessageType, payload interface{}) error {
+	tests.Info("Sending Command: %d - Data: %+q", messageType, payload)
+
+	var bu bytes.Buffer
+
+	if err := json.NewEncoder(&bu).Encode(server.Message{
+		Payload: payload,
+		Type:    messageType,
+	}); err != nil {
+		return err
+	}
+
+	return conn.WriteMessage(websocket.BinaryMessage, bu.Bytes())
+}
+
+// read reads the incoming response from the websocket connection.
+func read(conn *websocket.Conn) (server.Message, error) {
+	var message server.Message
+
+	_, msg, err := conn.ReadMessage()
+	if err != nil {
+		return server.Message{}, err
+	}
+
+	var bu bytes.Buffer
+	bu.Write(msg)
+
+	if err := json.NewDecoder(&bu).Decode(&message); err != nil {
+		return server.Message{}, err
+	}
+
+	return message, nil
 }
