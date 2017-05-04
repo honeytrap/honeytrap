543b82ce98099bceb204f27eeb71c2727e4dfeda
Alexander Ewetumo <trinoxf@gmail.com>
2017-05-04 13:10:36 +0100

    Add cowrie config sample changes and logs

--- director/iodirector/iodirector.go
+++ director/iodirector/iodirector.go
@@ -13,6 +13,7 @@ import (
 	"github.com/honeytrap/honeytrap/director"
 	"github.com/honeytrap/honeytrap/pushers"
 	"github.com/honeytrap/namecon"
+	logging "github.com/op/go-logging"
 )
 
 const (
@@ -22,6 +23,7 @@ const (
 
 var (
 	dailTimeout = 5 * time.Second
+	log         = logging.MustGetLogger("honeytrap:director:io")
 )
 
 // Director defines a central structure which creates/retrieves Container
@@ -46,11 +48,14 @@ func New(config *config.Config, events pushers.Events) *Director {
 
 // NewContainer returns a new Container generated from the director with the specified address.
 func (d *Director) NewContainer(addr string) (director.Container, error) {
+	log.Infof("IO : Creating new container : %s", addr)
+
 	var err error
 	var container director.Container
 
 	name, err := d.getName(addr)
 	if err != nil {
+		log.Errorf("IO : Failed to make new container name : %+q", err)
 		return nil, err
 	}
 
@@ -80,6 +85,8 @@ func (d *Director) NewContainer(addr string) (director.Container, error) {
 
 // GetContainer returns a new Container using the provided net.Conn if already registered.
 func (d *Director) GetContainer(conn net.Conn) (director.Container, error) {
+	log.Infof("IO : Attempt to retrieve existing container : %+q", conn.RemoteAddr())
+
 	var container director.Container
 
 	name, err := d.getName(conn.RemoteAddr().String())
@@ -122,6 +129,8 @@ type IOContainer struct {
 // Dial connects to the giving address to provide proxying stream between
 // both endpoints.
 func (io *IOContainer) Dial() (net.Conn, error) {
+	log.Infof("IO : %q : Dial Connection : Remote : %+q", io.targetName, io.meta.ServiceAddr)
+
 	conn, err := net.DialTimeout("tcp", io.meta.ServiceAddr, dailTimeout)
 	if err != nil {
 		return nil, err
