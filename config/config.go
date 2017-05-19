a05de29bc6dccc15f9cf61713bea63830d5b7471
Remco Verhoef <remco@dutchcoders.io>
2017-05-19 15:46:28 +0200

    Moved delay to own file

--- config/config.go
+++ config/config.go
@@ -118,28 +118,6 @@ type SMTPProxyConfig struct {
 	} `toml:"tls"`
 }
 
-// Delay defines a duration type.
-type Delay time.Duration
-
-// Duration returns the type of the giving duration from the provided pointer.
-func (t *Delay) Duration() time.Duration {
-	return time.Duration(*t)
-}
-
-// UnmarshalText handles unmarshalling duration values from the provided slice.
-func (t *Delay) UnmarshalText(text []byte) error {
-	s := string(text)
-
-	d, err := time.ParseDuration(s)
-	if err != nil {
-		log.Errorf("Error parsing duration (%s): %s", s, err.Error())
-		return err
-	}
-
-	*t = Delay(d)
-	return nil
-}
-
 // DefaultConfig defines the default Config to be used to set default values.
 var DefaultConfig = Config{
 	Token:     "",
