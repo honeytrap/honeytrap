5bb39438c36974c8fb6d65aa569efe95ef969b65
Alexander Ewetumo <trinoxf@gmail.com>
2017-06-23 13:42:55 +0100

    Refactor map into go1.9 and go1.8 as needed

--- pushers/event/event.go
+++ pushers/event/event.go
@@ -5,10 +5,6 @@ import (
 	"fmt"
 	"net"
 	"runtime/debug"
-
-	"time"
-
-	"golang.org/x/sync/syncmap"
 )
 
 //====================================================================================
@@ -254,68 +250,5 @@ func CopyFrom(data map[string]interface{}) Option {
 func Custom(name string, value interface{}) Option {
 	return func(m Event) {
 		m.Store(name, value)
-	}
-}
-
-//===============================================================================
-
-// Event defines a object which adds key-value pairs into a map type for event data.
-type Event struct {
-	sm *syncmap.Map
-}
-
-// New returns a new Event with the options applied.
-func New(opts ...Option) Event {
-	e := Event{
-		sm: new(syncmap.Map),
-	}
-
-	e.sm.Store("date", time.Now())
-
-	for _, opt := range opts {
-		opt(e)
-	}
-
-	return e
-}
-
-// Range defines a function which ranges the underline key-values with
-// the provided syncmap.
-func (e Event) Range(fx func(interface{}, interface{}) bool) {
-	e.sm.Range(fx)
-}
-
-// Store adds the key and value into the event.
-func (e Event) Store(s string, v interface{}) {
-	e.sm.Store(s, v)
-}
-
-// Map returns the underline map for the giving object.
-// 	mp := make(map[string]interface{})
-
-// 	e.sm.Range(func(key, value interface{}) bool {
-// 		if keyName, ok := key.(string); ok {
-// 			mp[keyName] = value
-// 		}
-
-// 		return true
-// 	})
-
-// 	return Map(mp)
-
-// Has returns true/false if the giving key exists.
-func (e Event) Has(s string) bool {
-	_, ok := e.sm.Load(s)
-	return ok
-}
-
-// Get retrieves a giving value for a key has string.
-func (e Event) Get(s string) string {
-	if v, ok := e.sm.Load(s); !ok {
-		return ""
-	} else if v, ok := v.(string); !ok {
-		return ""
-	} else {
-		return v
 	}
 }
