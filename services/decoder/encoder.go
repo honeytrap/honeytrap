1da1a1bb30b77ebb77d27ae634d2359839ec365b
Jerry Stevens <jerry@jerrystevens.nl>
2017-12-19 18:43:11 +0100

    initial

---
+++ services/decoder/encoder.go
@@ -0,0 +1,39 @@
+package decoder
+
+import (
+	"bytes"
+	"encoding/binary"
+)
+
+type EncoderType interface {
+	WriteUint8(b byte)
+	WriteUint16(v int16)
+	WriteUint32(v int32)
+	WriteData(v string)
+}
+
+type Encoder struct {
+	bytes.Buffer
+}
+
+func (e *Encoder) WriteUint8(b byte) {
+	_ = e.WriteByte(b)
+}
+
+func (e *Encoder) WriteUint16(v int16) {
+	b := [2]byte{}
+	binary.BigEndian.PutUint16(b[:], uint16(v))
+	e.Write(b[:])
+}
+
+func (e *Encoder) WriteUint32(v int32) {
+	b := [4]byte{}
+	binary.BigEndian.PutUint32(b[:], uint32(v))
+	e.Write(b[:])
+}
+
+func (e *Encoder) WriteData(v string) {
+
+	e.WriteUint16(int16(len(v)))
+	e.Write([]byte(v))
+}
