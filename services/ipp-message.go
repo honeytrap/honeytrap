07726becc745595d40cddb9e4f7a6c6ebae48baf
Jerry Stevens <jerry@jerrystevens.nl>
2017-12-19 12:02:52 +0100

    services: ipp: fixed reading requests and improved response

--- services/ipp-message.go
+++ services/ipp-message.go
@@ -60,8 +60,7 @@ type ippMessage struct {
 
 type attribGroup struct {
 	attribGroupTag byte //begin-attribute-group-tag
-	val            attribOneValue
-	additionalVal  []additionalValue
+	val            []attribOneValue
 }
 
 type attribOneValue struct { //Atrribute-with-one-value
@@ -70,6 +69,7 @@ type attribOneValue struct { //Atrribute-with-one-value
 	name     []byte
 	valueLen int16 //value-length
 	value    []byte
+	aVal     []additionalValue
 }
 
 type additionalValue struct { //additional-value
@@ -79,13 +79,39 @@ type additionalValue struct { //additional-value
 	value    []byte
 }
 
+func (ao attribOneValue) decode(dec decoder.Decoder) error {
+	var err error
+
+	vtag, err := dec.Byte()
+	nlen, err := dec.Int16()
+	if nlen == 0 { //Additional value
+		vlen, err := dec.Int16()
+		v := dec.Copy(int(vlen))
+		a := additionalValue{
+			valueTag: vtag,
+			nameLen:  nlen,
+			valueLen: vlen,
+			value:    v,
+		}
+		ao.aVal = append(ao.aVal, a)
+		return err
+	} else {
+		ao.valueTag = vtag
+		ao.nameLen = nlen
+		ao.name = dec.Copy(int(ao.nameLen))
+		ao.valueLen, err = dec.Int16()
+		ao.value = dec.Copy(int(ao.valueLen))
+	}
+	return err
+}
+
 // Returns a IPP response based on the IPP request
 func ippHandler(ippBody []byte) (*bytes.Buffer, []byte) {
 	body := &ippMessage{}
 
 	err := body.Read(ippBody)
 	if err != nil {
-		return nil
+		return nil, nil
 	}
 
 	rbody := &ippMessage{}
@@ -94,11 +120,11 @@ func ippHandler(ippBody []byte) (*bytes.Buffer, []byte) {
 	rbody.requestId = body.requestId
 	rbody.statusCode = sOk //We have the ultimate printer
 
-	var print []byte //nil slice, the print data if any
+	print := []byte{}
 
 	switch body.statusCode { //operation-id
 	case opPrintJob:
-		print := body.data
+		print = body.data
 	case opValidateJob:
 	case opCreateJob:
 	case opGetJobAttrib:
@@ -106,7 +132,10 @@ func ippHandler(ippBody []byte) (*bytes.Buffer, []byte) {
 	default:
 	}
 
-	return rbody.Response(), print
+	if len(print) > 0 {
+		return rbody.Response(), print
+	}
+	return rbody.Response(), nil
 }
 
 func (m *ippMessage) Read(raw []byte) error {
@@ -119,12 +148,29 @@ func (m *ippMessage) Read(raw []byte) error {
 	m.statusCode, _ = dec.Int16()
 	m.requestId, _ = dec.Int32()
 
-	//Find the end tag
-	for end, err := dec.Byte(); end == endAttribTag; end, err = dec.Byte() {
+	for dtag, err := dec.Byte(); dtag != endAttribTag; dtag, err = dec.Byte() {
 		if err != nil {
-			break
+			return err
 		}
+
+		group := attribGroup{}
+		group.attribGroupTag = dtag
+		for dp, err := dec.PeekByte(); dp > unsupAttribTag; dp, err = dec.PeekByte() { //Not a delimiter tag
+			if err != nil {
+				return err
+			}
+
+			aov := attribOneValue{}
+			if err := aov.decode(dec); err != nil {
+				return err
+			}
+			group.val = append(group.val, aov)
+		}
+
+		m.attributes = append(m.attributes, group)
 	}
+
+	m.endTag = endAttribTag
 	m.data = dec.Copy(dec.Available())
 
 	return nil
@@ -132,10 +178,31 @@ func (m *ippMessage) Read(raw []byte) error {
 
 func (m *ippMessage) Response() *bytes.Buffer {
 	buf := new(bytes.Buffer)
-	_ := binary.Write(buf, binary.BigEndian, m.versionMajor)
-	_ := binary.Write(buf, binary.BigEndian, m.versionMinor)
-	_ := binary.Write(buf, binary.BigEndian, m.statusCode)
-	_ := binary.Write(buf, binary.BigEndian, m.requestId)
+	var err error
+	err = binary.Write(buf, binary.BigEndian, m.versionMajor)
+	err = binary.Write(buf, binary.BigEndian, m.versionMinor)
+	err = binary.Write(buf, binary.BigEndian, m.statusCode)
+	err = binary.Write(buf, binary.BigEndian, m.requestId)
+	if m.attributes[0].attribGroupTag == opAttribTag {
+		err = binary.Write(buf, binary.BigEndian, opAttribTag)
+		//write charset
+		err = binary.Write(buf, binary.BigEndian, m.attributes[0].val[0].valueTag)
+		err = binary.Write(buf, binary.BigEndian, m.attributes[0].val[0].nameLen)
+		err = binary.Write(buf, binary.BigEndian, m.attributes[0].val[0].name)
+		err = binary.Write(buf, binary.BigEndian, m.attributes[0].val[0].valueLen)
+		err = binary.Write(buf, binary.BigEndian, m.attributes[0].val[0].value)
 
+		//write language
+		err = binary.Write(buf, binary.BigEndian, m.attributes[0].val[1].valueTag)
+		err = binary.Write(buf, binary.BigEndian, m.attributes[0].val[1].nameLen)
+		err = binary.Write(buf, binary.BigEndian, m.attributes[0].val[1].name)
+		err = binary.Write(buf, binary.BigEndian, m.attributes[0].val[1].valueLen)
+		err = binary.Write(buf, binary.BigEndian, m.attributes[0].val[1].value)
+	}
+
+	err = binary.Write(buf, binary.BigEndian, endAttribTag)
+	if err != nil {
+		return nil
+	}
 	return buf
 }
