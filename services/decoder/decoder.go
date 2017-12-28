b1c44649fad71f2089fdf2ed47e1ca233ae8061b
Jerry Stevens <jerry@jerrystevens.nl>
2017-12-28 01:04:56 +0100

    services: ipp: Added IPP values for decoding

--- services/decoder/decoder.go
+++ services/decoder/decoder.go
@@ -15,32 +15,48 @@ func (e ErrOutOfBounds) Error() string {
 	return fmt.Sprintf("Index out of bounds! min: %v, max: %v got: %v", e.Min, e.Max, e.Got)
 }
 
-func NewDefaultDecoder(data []byte, byteOrder binary.ByteOrder) *DefaultDecoder {
-	return &DefaultDecoder{
-		offset:      0,
-		startOffset: 0,
-		byteOrder:   byteOrder,
-		data:        data,
+type Decoder interface {
+	Available() int
+	HasBytes(size int) error
+
+	Byte() byte
+	Int16() int16
+	Int32() int32
+
+	PeekByte() byte
+	PeekInt16() int16
+
+	Data() string
+	Copy(size int) []byte
+	Seek(pos int)
+
+	LastError() error
+}
+
+type Decode struct {
+	offset    int
+	data      []byte
+	lasterror error
+}
+
+func NewDecoder(data []byte) *Decode {
+	return &Decode{
+		offset: 0,
+		data:   data,
 	}
 }
 
-type DefaultDecoder struct {
-	offset      int
-	startOffset int
-	byteOrder   binary.ByteOrder
-	data        []byte
+func (d *Decode) LastError() error {
+	return d.lasterror
+
 }
 
-func (d *DefaultDecoder) Available() int {
+func (d *Decode) Available() int {
 	return len(d.data) - d.offset
 
 }
 
-/* Check if size bytes are available
- * Zero or negative sizes are not allowed
- * nb. Use Seek if you wanna go back
- */
-func (d *DefaultDecoder) HasBytes(size int) error {
+func (d *Decode) HasBytes(size int) error {
 	if size > 0 && len(d.data) >= d.offset+size {
 		return nil
 	}
@@ -52,83 +68,88 @@ func (d *DefaultDecoder) HasBytes(size int) error {
 	}
 }
 
-func (d *DefaultDecoder) Byte() (byte, error) {
+func (d *Decode) Byte() byte {
 	if err := d.HasBytes(1); err != nil {
-		return 0, err
+		d.lasterror = err
+		return 0
 	}
 
 	defer func() {
 		d.offset += 1
 	}()
 
-	return d.data[d.offset], nil
+	return d.data[d.offset]
 }
 
-func (d *DefaultDecoder) Int16() (int16, error) {
+func (d *Decode) Int16() int16 {
 	if err := d.HasBytes(2); err != nil {
-		return 0, err
+		d.lasterror = err
+		return 0
 	}
 
 	defer func() {
 		d.offset += 2
 	}()
 
-	return int16(d.byteOrder.Uint16(d.data[d.offset : d.offset+2])), nil
+	return int16(binary.BigEndian.Uint16(d.data[d.offset : d.offset+2]))
 }
 
-func (d *DefaultDecoder) Int32() (int32, error) {
+func (d *Decode) Int32() int32 {
 	if err := d.HasBytes(4); err != nil {
-		return 0, err
+		d.lasterror = err
+		return 0
 	}
 
 	defer func() {
 		d.offset += 4
 	}()
 
-	return int32(d.byteOrder.Uint32(d.data[d.offset : d.offset+4])), nil
+	return int32(binary.BigEndian.Uint32(d.data[d.offset : d.offset+4]))
 }
 
-func (d *DefaultDecoder) Data() (string, error) {
-	l, err := d.Int16()
-	return string(d.Copy(int(l))), err
+func (d *Decode) Data() string {
+	l := d.Int16()
+	return string(d.Copy(int(l)))
 }
 
-func (d *DefaultDecoder) PeekByte() (byte, error) {
+func (d *Decode) PeekByte() byte {
 	if err := d.HasBytes(1); err != nil {
-		return 0, err
+		d.lasterror = err
+		return 0
 	}
 
-	return d.data[d.offset], nil
+	return d.data[d.offset]
 }
 
-/* Copy size bytes from offset to new byte slice
- * Returns nil if size is 0 or larger then data size.
- * offset is set accordingly
- */
-func (d *DefaultDecoder) Copy(size int) []byte {
-	if size < 0 || d.offset+size > len(d.data) {
+func (d *Decode) PeekInt16() int16 {
+	if err := d.HasBytes(2); err != nil {
+		d.lasterror = err
+		return 0
+	}
+
+	return int16(binary.BigEndian.Uint16(d.data[d.offset : d.offset+2]))
+}
+
+func (d *Decode) Copy(size int) []byte {
+	if err := d.HasBytes(size); err != nil {
+		d.lasterror = err
 		return nil
 	}
+
 	c := make([]byte, size)
 	copy(c, d.data[d.offset:d.offset+size])
 	d.offset += size
 	return c
 }
 
-/* Seeks to absolute position,
- * Error if n gets out of bounds
- *   offset remains unchanged
- */
-func (d *DefaultDecoder) Seek(n int) (int, error) {
-	prev := d.offset
-
-	if n < 0 || n >= len(d.data) {
-		return prev, ErrOutOfBounds{
-			Min: d.startOffset,
-			Max: len(d.data) - 1,
-			Got: n,
+// Seeking relative to current offset
+func (d *Decode) Seek(pos int) {
+	d.offset += pos
+	if d.offset < 0 || d.offset > len(d.data) {
+		d.lasterror = ErrOutOfBounds{
+			Min: 0,
+			Max: len(d.data),
+			Got: d.offset,
 		}
 	}
-	d.offset = n
-	return prev, nil
 }
