2d53ad58833df2c3ac92b453ee1bbfdffbf2ee23
Jerry Stevens <jerry@jerrystevens.nl>
2018-03-20 12:30:16 +0100

    services/stringformatter: more comments

--- services/stringformatter/stringformatter.go
+++ services/stringformatter/stringformatter.go
@@ -29,8 +29,10 @@ func New(templ string) (*strFormat, error) {
 	}, nil
 }
 
-// template: `datetime: {{timefmt .Time "Jan 2 15:04:02 2006"}} Text: {{.VarText}}`
-// will render a string like this: "datetime Mar 19 14:17:19 2018 Text: sometext"
+// template: `datetime: {{timefmt .Time "<time format>"}} Text: {{.VarText}}`
+// An empty <time format> will render as RFC3339
+// Ex. `datetime: {{timefmt .Time "Jan 2 15:04:02 2006"}} Text: {{.VarText}}`
+//     renders as: "datetime Mar 19 14:17:19 2018 Text: sometext"
 func (s *strFormat) String(tm time.Time, vartext string) string {
 
 	var parsed strings.Builder
