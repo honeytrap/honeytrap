1585cd7936e8f7f71c50167599fa12bb59334d07
Remco Verhoef <remco@dutchcoders.io>
2018-04-28 14:50:14 +0200

    services: ssh-simulator: make max auth tries configurable

--- services/ssh/ssh-simulator.go
+++ services/ssh/ssh-simulator.go
@@ -80,9 +80,10 @@ func Simulator(options ...services.ServicerFunc) services.Servicer {
 	banner := "SSH-2.0-OpenSSH_6.6.1p1 2020Ubuntu-2ubuntu2"
 
 	service := &sshSimulatorService{
-		key:    s.PrivateKey(),
-		Banner: banner,
-		MOTD:   motd,
+		key:          s.PrivateKey(),
+		Banner:       banner,
+		MOTD:         motd,
+		MaxAuthTries: -1,
 		Credentials: []string{
 			"*",
 		},
@@ -100,6 +101,8 @@ type sshSimulatorService struct {
 
 	Banner string `toml:"banner"`
 	MOTD   string `toml:"motd"`
+
+	MaxAuthTries int `toml:"max-auth-tries"`
 
 	Credentials []string    `toml:"credentials"`
 	key         *privateKey `toml:"private-key"`
@@ -134,6 +137,7 @@ func (s *sshSimulatorService) Handle(ctx context.Context, conn net.Conn) error {
 
 	config := ssh.ServerConfig{
 		ServerVersion: s.Banner,
+		MaxAuthTries:  s.MaxAuthTries,
 		PublicKeyCallback: func(cm ssh.ConnMetadata, key ssh.PublicKey) (*ssh.Permissions, error) {
 			s.c.Send(event.New(
 				services.EventOptions,
