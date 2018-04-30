ed9559c9921a8a28f6c5d6e6fbfa52a637c34c8c
Remco Verhoef <remco@dutchcoders.io>
2018-04-30 23:34:06 +0200

    services: ethereum: added extra methods

--- services/ethereum/ethereum.go
+++ services/ethereum/ethereum.go
@@ -46,11 +46,14 @@ import (
 	"github.com/honeytrap/honeytrap/event"
 	"github.com/honeytrap/honeytrap/pushers"
 	"github.com/honeytrap/honeytrap/services"
+	logging "github.com/op/go-logging"
 )
 
 var (
 	_ = services.Register("ethereum", Ethereum)
 )
+
+var log = logging.MustGetLogger("honeytrap/services/ethereum")
 
 // Ethereum is a placeholder
 func Ethereum(options ...services.ServicerFunc) services.Servicer {
@@ -87,6 +90,57 @@ func Headers(headers map[string][]string) event.Option {
 			m.Store(fmt.Sprintf("http.header.%s", strings.ToLower(name)), h)
 		}
 	}
+}
+
+var ethereumMethods = map[string]func(map[string]interface{}) map[string]interface{}{
+	"eth_accounts": func(m map[string]interface{}) map[string]interface{} {
+		return map[string]interface{}{
+			"id":      m["id"],
+			"jsonrpc": m["jsonrpc"],
+			"result":  []string{"0x407d73d8a49eeb85d32cf465507dd71d507100c1"},
+		}
+	},
+	"eth_blockNumber": func(m map[string]interface{}) map[string]interface{} {
+		return map[string]interface{}{
+			"id":      m["id"],
+			"jsonrpc": m["jsonrpc"],
+			"result":  "0x4b7",
+		}
+	},
+	"web3_clientVersion": func(m map[string]interface{}) map[string]interface{} {
+		return map[string]interface{}{
+			"id":      m["id"],
+			"jsonrpc": m["jsonrpc"],
+			"result":  "Mist/v0.9.3/darwin/go1.4.1",
+		}
+	},
+	"eth_getBlockByNumber": func(m map[string]interface{}) map[string]interface{} {
+		return map[string]interface{}{
+			"id":      m["id"],
+			"jsonrpc": m["jsonrpc"],
+			"result": map[string]interface{}{
+				"number":           "0x1b4",
+				"hash":             "0xe670ec64341771606e55d6b4ca35a1a6b75ee3d5145a99d05921026d1527331",
+				"parentHash":       "0x9646252be9520f6e71339a8df9c55e4d7619deeb018d2a3f2d21fc165dde5eb5",
+				"nonce":            "0xe04d296d2460cfb8472af2c5fd05b5a214109c25688d3704aed5484f9a7792f2",
+				"sha3Uncles":       "0x1dcc4de8dec75d7aab85b567b6ccd41ad312451b948a7413f0a142fd40d49347",
+				"logsBloom":        "0xe670ec64341771606e55d6b4ca35a1a6b75ee3d5145a99d05921026d1527331",
+				"transactionsRoot": "0x56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421",
+				"stateRoot":        "0xd5855eb08b3387c0af375e9cdb6acfc05eb8f519e419b874b6ff2ffda7ed1dff",
+				"miner":            "0x4e65fda2159562a496f9f3522f89122a3088497a",
+				"difficulty":       "0x027f07",
+				"totalDifficulty":  "0x027f07",
+				"extraData":        "0x0000000000000000000000000000000000000000000000000000000000000000",
+				"size":             "0x027f07",
+				"gasLimit":         "0x9f759",
+				"minGasPrice":      "0x9f759",
+				"gasUsed":          "0x9f759",
+				"timestamp":        "0x54e34e8e",
+				"transactions":     []interface{}{},
+				"uncles":           []interface{}{},
+			},
+		}
+	},
 }
 
 func (s *ethereumService) Handle(ctx context.Context, conn net.Conn) error {
@@ -135,6 +189,19 @@ func (s *ethereumService) Handle(ctx context.Context, conn net.Conn) error {
 		Headers(req.Header),
 	))
 
+	buff := bytes.Buffer{}
+
+	fn, ok := ethereumMethods[method]
+	if ok {
+		v := fn(jsonRequest)
+
+		if err := json.NewEncoder(&buff).Encode(v); err != nil {
+			return err
+		}
+	} else {
+		log.Errorf("Method %s not supported", method)
+	}
+
 	resp := http.Response{
 		StatusCode: http.StatusOK,
 		Status:     http.StatusText(http.StatusOK),
@@ -145,37 +212,8 @@ func (s *ethereumService) Handle(ctx context.Context, conn net.Conn) error {
 		Header:     http.Header{},
 	}
 
-	buff := bytes.Buffer{}
-
-	if err := json.NewEncoder(&buff).Encode(map[string]interface{}{
-		"id":      jsonRequest["id"],
-		"jsonrpc": jsonRequest["jsonrpc"],
-		"result": map[string]interface{}{
-			"number":           "0x1b4",
-			"hash":             "0xe670ec64341771606e55d6b4ca35a1a6b75ee3d5145a99d05921026d1527331",
-			"parentHash":       "0x9646252be9520f6e71339a8df9c55e4d7619deeb018d2a3f2d21fc165dde5eb5",
-			"nonce":            "0xe04d296d2460cfb8472af2c5fd05b5a214109c25688d3704aed5484f9a7792f2",
-			"sha3Uncles":       "0x1dcc4de8dec75d7aab85b567b6ccd41ad312451b948a7413f0a142fd40d49347",
-			"logsBloom":        "0xe670ec64341771606e55d6b4ca35a1a6b75ee3d5145a99d05921026d1527331",
-			"transactionsRoot": "0x56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421",
-			"stateRoot":        "0xd5855eb08b3387c0af375e9cdb6acfc05eb8f519e419b874b6ff2ffda7ed1dff",
-			"miner":            "0x4e65fda2159562a496f9f3522f89122a3088497a",
-			"difficulty":       "0x027f07",
-			"totalDifficulty":  "0x027f07",
-			"extraData":        "0x0000000000000000000000000000000000000000000000000000000000000000",
-			"size":             "0x027f07",
-			"gasLimit":         "0x9f759",
-			"gasUsed":          "0x9f759",
-			"timestamp":        "0x54e34e8e",
-			"transactions":     map[string]interface{}{},
-			"uncles":           map[string]interface{}{},
-		},
-	}); err != nil {
-		return err
-	}
-
-	resp.Header.Add("content-type", "application/json; charset=UTF-8")
-	resp.Header.Add("content-length", fmt.Sprintf("%d", buff.Len()))
+	resp.Header.Add("Content-Type", "application/json; charset=UTF-8")
+	resp.Header.Add("Content-Length", fmt.Sprintf("%d", buff.Len()))
 
 	resp.Body = ioutil.NopCloser(&buff)
 
