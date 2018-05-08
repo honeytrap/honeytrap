0fb9dd0b05383b8e2a2aa3ea8af085a827bf7568
Nordin van Nes <N.vanNes@student.tudelft.nl>
2018-05-08 15:41:02 +0200

    Feature lua float function (#7)
    * Update lua string functions
    
    * Update integer function lua
    
    * Remove duplicate function
    
    * Update comment on function
    
    * Add service name to SetFloatFunction
    
    * Fixed Floatfunction to new connectionStruct

--- scripter/scripter.go
+++ scripter/scripter.go
@@ -42,6 +42,7 @@ type Scripter interface {
 type ConnectionWrapper interface {
 	Handle(message string) (string, error)
 	SetStringFunction(name string, getString func() string) error
+	SetFloatFunction(name string, getFloat func() float64) error
 }
 
 func WithConfig(c toml.Primitive) func(Scripter) error {
