0424e217e3e5aeede3d4c65a4963055804e070cd
Remco Verhoef <remco@dutchcoders.io>
2018-07-03 22:13:51 +0200

    Revert "services/ldap: new server. removed go 1.9 from travis"
    This reverts commit e26d5fc59f2318181b470732218cd214abd26c5f.

--- server/honeytrap.go
+++ server/honeytrap.go
@@ -63,7 +63,6 @@ import (
 	_ "github.com/honeytrap/honeytrap/services/ethereum"
 	_ "github.com/honeytrap/honeytrap/services/ftp"
 	_ "github.com/honeytrap/honeytrap/services/ipp"
-	_ "github.com/honeytrap/honeytrap/services/ldap"
 	_ "github.com/honeytrap/honeytrap/services/redis"
 	_ "github.com/honeytrap/honeytrap/services/smtp"
 	_ "github.com/honeytrap/honeytrap/services/ssh"
