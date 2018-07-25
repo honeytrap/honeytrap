617d9c00eae7cb7d62c008f9808287aac3a0c277
CapacitorSet <capacitorset@gmail.com>
2018-07-25 12:47:38 +0200

    Extract transform

--- transforms/yara/yara.go
+++ transforms/yara/yara.go
@@ -12,7 +12,6 @@ import (
 
 	goyara "github.com/hillu/go-yara"
 	"github.com/honeytrap/honeytrap/event"
-	"github.com/honeytrap/honeytrap/transforms"
 	"github.com/honeytrap/yara-parser/data"
 	"github.com/honeytrap/yara-parser/grammar"
 	"github.com/op/go-logging"
@@ -190,36 +189,4 @@ func (m Matcher) MustMatch(e event.Event) bool {
 		panic(err)
 	}
 	return len(matches) > 0
-}
-
-func Yara(source string) transforms.TransformFunc {
-	c, err := NewCompiler()
-	if err != nil {
-		panic(err)
-	}
-	err = c.AddRulesFrom(source)
-	if err != nil {
-		panic(err)
-	}
-	m, err := NewMatcher(c)
-	if err != nil {
-		panic(err)
-	}
-	return func(state transforms.State, e event.Event, send func(event.Event)) {
-		matches, err := m.GetMatches(e)
-		if err != nil {
-			panic(err.Error())
-			log.Error(err.Error())
-			return
-		}
-		for _, match := range matches {
-			// Duplicate the event and add Yara metadata
-			extendedEvt := event.New(
-				event.MergeFrom(event.ToMap(e)),
-				event.Custom("yara.rule", match.Rule),
-				event.Custom("yara.tags", strings.Join(match.Tags, ",")),
-			)
-			send(extendedEvt)
-		}
-	}
 }
