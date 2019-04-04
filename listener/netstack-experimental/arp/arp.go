c0d81e0e8667c1187d473754adfd936db25da6a1
Remco Verhoef <remco@dutchcoders.io>
2019-04-04 11:44:41 +0200

    relicense from AGPL to Apache 2.0 license

--- listener/netstack-experimental/arp/arp.go
+++ listener/netstack-experimental/arp/arp.go
@@ -1,28 +1,16 @@
-// Copyright 2018 Google LLC
+// Copyright 2016-2019 DutchSec (https://dutchsec.com/)
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+// http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
-// Package arp implements the ARP network protocol. It is used to resolve
-// IPv4 addresses into link-local MAC addresses, and advertises IPv4
-// addresses of its stack with the local network.
-//
-// To use it in the networking stack, pass arp.ProtocolName as one of the
-// network protocols when calling stack.New. Then add an "arp" address to
-// every NIC on the stack that should respond to ARP requests. That is:
-//
-//	if err := s.AddAddress(1, arp.ProtocolNumber, "arp"); err != nil {
-//		// handle err
-//	}
 package arp
 
 import (
